cd7ec008d9524589d009ccbc7167ee2f2cdf5099
ibrahimBOU <85347626+ibrahimBOU@users.noreply.github.com>
2022-10-25 11:54:23 +0200

    update

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ const contentContainer = document.querySelector('#contentApp')
 
 const router = new Router(menus, contentContainer)
 router.start()
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ibrahimbounab:9BP2LeaOfA6b9nQg@cluster0.zlwvxyx.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
